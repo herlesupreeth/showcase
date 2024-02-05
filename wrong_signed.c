cd2f36fc05230faf453e3e822483f88499b422cb
syc2012 <87954959+syc2012@users.noreply.github.com>
2024-02-05 20:22:12 +0800

    Update wrong_signed.c

--- wrong_signed.c
+++ wrong_signed.c
@@ -1,5 +1,165 @@
 #include <stdio.h>
 
+
+void add1(int a, int b)
+{
+    int c;
+
+    printf("a = %d (0x%x)\n", a, a);
+    printf("b = %d (0x%x)\n", b, b);
+
+    c = a + b;
+
+    printf("c = %d + %d = %d (0x%x)\n", a, b, c, c);
+    printf("\n");
+}
+
+void add2(unsigned int a, int b)
+{
+    int c;
+
+    printf("a = %d (0x%x)\n", a, a);
+    printf("b = %d (0x%x)\n", b, b);
+
+    c = a + b;
+
+    printf("c = %d + %d = %d (0x%x)\n", a, b, c, c);
+    printf("\n");
+}
+
+void add3(int a, unsigned int b)
+{
+    int c;
+
+    printf("a = %d (0x%x)\n", a, a);
+    printf("b = %d (0x%x)\n", b, b);
+
+    c = a + b;
+
+    printf("c = %d + %d = %d (0x%x)\n", a, b, c, c);
+    printf("\n");
+}
+
+void add4(unsigned int a, unsigned int b)
+{
+    int c;
+
+    printf("a = %d (0x%x)\n", a, a);
+    printf("b = %d (0x%x)\n", b, b);
+
+    c = a + b;
+
+    printf("c = %d + %d = %d (0x%x)\n", a, b, c, c);
+    printf("\n");
+}
+
+
+void sub1(int a, int b)
+{
+    int c;
+
+    printf("a = %d (0x%x)\n", a, a);
+    printf("b = %d (0x%x)\n", b, b);
+
+    c = a - b;
+
+    printf("c = %d - %d = %d (0x%x)\n", a, b, c, c);
+    printf("\n");
+}
+
+void sub2(unsigned int a, int b)
+{
+    int c;
+
+    printf("a = %d (0x%x)\n", a, a);
+    printf("b = %d (0x%x)\n", b, b);
+
+    c = a - b;
+
+    printf("c = %d - %d = %d (0x%x)\n", a, b, c, c);
+    printf("\n");
+}
+
+void sub3(int a, unsigned int b)
+{
+    int c;
+
+    printf("a = %d (0x%x)\n", a, a);
+    printf("b = %d (0x%x)\n", b, b);
+
+    c = a - b;
+
+    printf("c = %d - %d = %d (0x%x)\n", a, b, c, c);
+    printf("\n");
+}
+
+void sub4(unsigned int a, unsigned int b)
+{
+    int c;
+
+    printf("a = %d (0x%x)\n", a, a);
+    printf("b = %d (0x%x)\n", b, b);
+
+    c = a - b;
+
+    printf("c = %d - %d = %d (0x%x)\n", a, b, c, c);
+    printf("\n");
+}
+
+
+void mul1(int a, int b)
+{
+    int c;
+
+    printf("a = %d (0x%x)\n", a, a);
+    printf("b = %d (0x%x)\n", b, b);
+
+    c = a * b;
+
+    printf("c = %d * %d = %d (0x%x)\n", a, b, c, c);
+    printf("\n");
+}
+
+void mul2(unsigned int a, int b)
+{
+    int c;
+
+    printf("a = %d (0x%x)\n", a, a);
+    printf("b = %d (0x%x)\n", b, b);
+
+    c = a * b;
+
+    printf("c = %d * %d = %d (0x%x)\n", a, b, c, c);
+    printf("\n");
+}
+
+void mul3(int a, unsigned int b)
+{
+    int c;
+
+    printf("a = %d (0x%x)\n", a, a);
+    printf("b = %d (0x%x)\n", b, b);
+
+    c = a * b;
+
+    printf("c = %d * %d = %d (0x%x)\n", a, b, c, c);
+    printf("\n");
+}
+
+void mul4(unsigned int a, unsigned int b)
+{
+    int c;
+
+    printf("a = %d (0x%x)\n", a, a);
+    printf("b = %d (0x%x)\n", b, b);
+
+    c = a * b;
+
+    printf("c = %d * %d = %d (0x%x)\n", a, b, c, c);
+    printf("\n");
+}
+
+
 void div1(int a, int b)
 {
     int c;
@@ -52,39 +212,61 @@ void div4(unsigned int a, unsigned int b)
     printf("\n");
 }
 
-int main(void)
+
+void (*g_fnc1[])(int, int) = {
+    add1, sub1, mul1, div1
+};
+
+void (*g_fnc2[])(unsigned int, int) = {
+    add2, sub2, mul2, div2
+};
+
+void (*g_fnc3[])(int, unsigned int) = {
+    add3, sub3, mul3, div3
+};
+
+void (*g_fnc4[])(unsigned int, unsigned int) = {
+    add4, sub4, mul4, div4
+};
+
+int main(int argc, char *argv[])
 {
-    printf("---------------------------------\n");
-    printf(" (int) / (int)\n");
-    printf("---------------------------------\n");
-    div1( 300,  6);
-    div1( 300, -6);
-    div1(-300,  6);
-    div1(-300, -6);
+    char symb[] = { '+', '-', '*', '/' };
+    int op = 3;
+
+    if (argc > 1) op = (atoi( argv[1] ) & 0x3);
 
     printf("---------------------------------\n");
-    printf(" (unsigned int) / (int)\n");
+    printf(" (int) %c (int)\n", symb[op]);
     printf("---------------------------------\n");
-    div2( 300,  6);
-    div2( 300, -6);
-    div2(-300,  6);
-    div2(-300, -6);
+    g_fnc1[op]( 300,  6);
+    g_fnc1[op]( 300, -6);
+    g_fnc1[op](-300,  6);
+    g_fnc1[op](-300, -6);
 
     printf("---------------------------------\n");
-    printf(" (int) / (unsigned int)\n");
+    printf(" (unsigned int) %c (int)\n", symb[op]);
     printf("---------------------------------\n");
-    div3( 300,  6);
-    div3( 300, -6);
-    div3(-300,  6);
-    div3(-300, -6);
+    g_fnc2[op]( 300,  6);
+    g_fnc2[op]( 300, -6);
+    g_fnc2[op](-300,  6);
+    g_fnc2[op](-300, -6);
 
     printf("---------------------------------\n");
-    printf(" (unsigned int) / (unsigned int)\n");
+    printf(" (int) %c (unsigned int)\n", symb[op]);
     printf("---------------------------------\n");
-    div4( 300,  6);
-    div4( 300, -6);
-    div4(-300,  6);
-    div4(-300, -6);
+    g_fnc3[op]( 300,  6);
+    g_fnc3[op]( 300, -6);
+    g_fnc3[op](-300,  6);
+    g_fnc3[op](-300, -6);
+
+    printf("---------------------------------\n");
+    printf(" (unsigned int) %c (unsigned int)\n", symb[op]);
+    printf("---------------------------------\n");
+    g_fnc4[op]( 300,  6);
+    g_fnc4[op]( 300, -6);
+    g_fnc4[op](-300,  6);
+    g_fnc4[op](-300, -6);
 
     return 0;
 }
